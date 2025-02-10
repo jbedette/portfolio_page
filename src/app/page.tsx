05e4d3a447ed1d60e75bff2dcd1b0a78c941ae7a
jbedette <john.bedette45@gmail.com>
2025-02-10 14:18:12 -0800

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,18 +21,24 @@ const projects: Project[] = [
   },
   {
     title: 'Virtualized and Containerized Secure Homelab',
-    description: 'FreeBSD vm Router protecting an Ubunter server vm ',
+    description: 'FreeBSD vm Router protecting an Ubuntu server vm ',
     link: `${githubAddr}prowl`,
     imageUrl: "images/wazuh_portal.png"
   },
   {
+    title: 'Haskell QuickCheck Analysis',
+    description: 'A deep dive evaluating, comparing, and contrasting Haskell\'s QuickCheck library',
+    link: `${githubAddr}haskell_quickcheck_examination`,
+    imageUrl: "images/haskell_qc.jpg"
+  },
+  {
     title: 'AI/ML Financial Sentiment Analysis',
-    description: 'Using 10-k reports taken from SEC-Edgar, analyze ',
+    description: 'Using 10-k reports taken from SEC-Edgar, analyze financial sentiment on S&P 500 companies',
     link: `${githubAddr}ai_10-k_analyzer`,
     imageUrl: "images/ai_fin_sent.jpg"
   },
   {
-    title: 'MTG momir format card printer',
+    title: 'Magic:The Gathering Momir Format Card Printer',
     description: 'Physical gadget that uses scryfall.com\'s api to generate a low resolution black and white MTG card, connect with a thermal printer via bluetooth credential spoofing and print it on a label',
     link: `${githubAddr}momir`,
     imageUrl: "images/momir_gui.png"
